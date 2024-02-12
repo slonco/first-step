42bb041c08287f51d51b02865e97fbbdca1860bc
Сергей Лонщаков <lonshchakov@MacBook-Pro-Lonshchakov.local>
2024-02-12 21:22:52 +0300

    it proger

---
+++ itproger.py
@@ -0,0 +1,29 @@
+# do not use $ # ^
+
+from time import sleep
+
+num1 = int(input("Введите первое число: "))
+
+num2 = int(input("Введите второе число: "))
+
+num1 *= 0
+
+print("Result: ", num1 + num2)
+print("Result: ", num1 * num2)
+print("Result: ", num1 - num2)
+print("Result: ", num1 / num2)
+
+# number = 5**2
+# digit = 7
+# text = "Результат: "
+# variable = True
+
+# print(text + str(number + digit))
+
+# print(text, number, end='!\n')
+#
+# sleep(1)
+#
+# number = 15
+#
+# print(text, number, end='!')
