f377466f3c2f2e2adc3393371147e9ed090432eb
Сергей Лонщаков <lonshchakov@MacBook-Pro-Lonshchakov.local>
2024-02-20 10:18:08 +0300

    new example from codewars

---
+++ codewars/example.py
@@ -0,0 +1,6 @@
+# You receive an array with your peers' test scores. Now calculate the average and compare your score!
+# Return True if you're better, else False!
+
+def better_than_average(class_points, your_points):
+    return True if (sum(class_points) + your_points)/(len(class_points)+1) < your_points else False
+
