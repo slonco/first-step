2c45e0239747e4d5f6812392a42ad874d5888333
Сергей Лонщаков <lonshchakov@MacBook-Pro-Lonshchakov.local>
2024-02-20 10:19:23 +0300

    new example from codewars + best way

--- codewars/example.py
+++ codewars/example.py
@@ -4,3 +4,6 @@
 def better_than_average(class_points, your_points):
     return True if (sum(class_points) + your_points)/(len(class_points)+1) < your_points else False
 
+# best way
+# def better_than_average(class_points, your_points):
+#     return your_points > sum(class_points) / len(class_points)
